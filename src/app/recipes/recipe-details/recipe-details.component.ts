e22963373874b35c6deba0f0a07db167c7ef3a38
Chris Bradley <chris.bradley.423@gmail.com>
2017-03-18 16:23:02 -0400

    add recipe detail structure

--- src/app/recipes/recipe-details/recipe-details.component.ts
+++ src/app/recipes/recipe-details/recipe-details.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'rb-recipe-details',
-  templateUrl: './recipe-details.component.html',
-  styleUrls: ['./recipe-details.component.css']
+  templateUrl: './recipe-details.component.html'
 })
 export class RecipeDetailsComponent implements OnInit {
 
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe';
+
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
+  recipe = new Recipe("Dummy", "This is a dummy recipe.", "https://placeimg.com/640/480/any", "Random image from placeimg.com");
 
   constructor() { }
 
